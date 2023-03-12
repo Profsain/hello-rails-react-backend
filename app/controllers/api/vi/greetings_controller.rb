b1fa267b120de4c6efdfea6df5d0d870d89d1b53
Profsain <profsaincrown@gmail.com>
2023-03-12 10:09:33 +0000

    fix rubocop

--- app/controllers/api/vi/greetings_controller.rb
+++ app/controllers/api/vi/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::Vi::GreetingsController < ApplicationController
   def random_greeting
-    @greeting = Greeting.order("RANDOM()").first
+    @greeting = Greeting.order('RANDOM()').first
     render json: @greeting
   end
 end
