021dd246c8a95166502808eede87a6ebc08b5969
Profsain <profsaincrown@gmail.com>
2023-03-17 09:28:16 +0000

    fix api error

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::GreetingsController < ApplicationController
+  def index
+    @greetings = Greeting.all
+    render json: @greetings
+  end
+  def random_greeting
+    @greeting = Greeting.order('RANDOM()').first
+    render json: @greeting
+  end
+end
